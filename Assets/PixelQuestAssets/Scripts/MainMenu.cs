0ee324466d4806af085204c4d6f019fc86e95640
stemguest <Omarw1621@gmail.com>
2024-11-23 12:05:13 -0500

    created new level

--- Assets/PixelQuestAssets/Scripts/MainMenu.cs
+++ Assets/PixelQuestAssets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string startLevel = "TestLevel"; // Level we are going to start the game from
+    public string startLevel = "World1"; // Level we are going to start the game from
 
     // Used by the UI button to load us into the given level 
     public void StartButton()
